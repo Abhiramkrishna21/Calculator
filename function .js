1799cdd756c4c26deb966515cd29349648ce5cb0
Abhiram krishna21 <abhiramkrishna328@gmail.com>
2023-09-13 10:27:38 +0530

    initial commit

---
+++ function .js
@@ -0,0 +1,12 @@
+function displaydat(data){
+    result.value+=data
+}
+function allclear(){
+    result.value=""
+}
+function findount(){
+    result.value=eval(result.value)
+}
+function backspace(){
+    result.value=result.value.slice(0,-1)
+}
